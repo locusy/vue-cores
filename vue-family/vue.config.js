2b785fb5479434797947c8a825ef7fc866b5c96e
tianzhi <1095098525@qq.com>
2020-03-02 23:00:59 +0800

    router

--- vue-family/vue.config.js
+++ vue-family/vue.config.js
@@ -2,5 +2,21 @@
  * @Author: tianzhi
  * @Date: 2020-03-02 17:04:17
  * @LastEditors: tianzhi
- * @LastEditTime: 2020-03-02 17:04:18
+ * @LastEditTime: 2020-03-02 17:16:20
  */
+module.exports = {
+    publicPath: '/family',
+    configureWebpack: {
+        devServer: {
+            before(app) {
+                // app是express 实例
+                app.get('/lists', (req, res) => {
+                    res.json([
+                        {id:1, text:'abc'},
+                        {id:2, text:'abc'},
+                    ])
+                })
+            }
+        }
+    }
+}
