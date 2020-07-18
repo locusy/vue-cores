1fd0a9c43e5774d8e1350fc1ccf9190ca4f22147
tianzhi <1095098525@qq.com>
2020-07-18 14:14:38 +0800

    vuex

--- vue-family/src/cstore.js
+++ vue-family/src/cstore.js
@@ -7,7 +7,7 @@ class CStore {
         this.actions = options.actions
 
         // 利用vue的数据监听机制
-        // 这是vuex和redux区别的一点 vuex只能在vue中使用 而redux可以在不同框架使用
+        // 这是vuex和redux区别的一点：vuex只能在vue中使用 而redux可以在不同框架使用
         new Vue({
             data: {
                 state: this.state
